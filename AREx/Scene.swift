24667c2376d04c17b5382a483fc91d15d75583a0
Michel <coolnerdcool@gmail.com>
2017-11-12 09:38:29 -0600

    feat(Scene): Created method to create the targets and a working timer.

--- AREx/Scene.swift
+++ AREx/Scene.swift
@@ -11,8 +11,34 @@ import ARKit
 
 class Scene: SKScene {
     
+    let remainingLabel = SKLabelNode()
+    var timer: Timer?
+    var targetsCreated = 0
+    var targetsCount = 0 {
+        didSet{
+            self.remainingLabel.text = "Faltan: \(targetsCount)"
+        }
+    }
+    
+    
+    
     override func didMove(to view: SKView) {
         // Setup your scene here
+        
+        // HUD config
+        remainingLabel.fontSize = 30
+        remainingLabel.fontName = "System"
+        remainingLabel.color = .white
+        remainingLabel.position = CGPoint(x: 0, y: view.frame.midY-50)
+        addChild(remainingLabel)
+        
+        targetsCount = 0
+        
+        //  Create enemies every 3 secs.
+        timer = Timer.scheduledTimer(withTimeInterval: 3.0, repeats: true, block: { (timer) in
+                                                                                self.createTarget()
+                                                                            }
+        )
     }
     
     override func update(_ currentTime: TimeInterval) {
@@ -20,21 +46,38 @@ class Scene: SKScene {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard let sceneView = self.view as? ARSKView else {
+        
+    }
+    
+    func createTarget(){
+        
+        if targetsCreated == 25{
+            timer?.invalidate()
+            timer = nil
             return
         }
         
-        // Create anchor using the camera's current position
-        if let currentFrame = sceneView.session.currentFrame {
-            
-            // Create a transform with a translation of 0.2 meters in front of the camera
-            var translation = matrix_identity_float4x4
-            translation.columns.3.z = -0.2
-            let transform = simd_mul(currentFrame.camera.transform, translation)
-            
-            // Add a new anchor to the session
-            let anchor = ARAnchor(transform: transform)
-            sceneView.session.add(anchor: anchor)
-        }
+        targetsCreated += 1
+        targetsCount += 1
+        
+        
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
